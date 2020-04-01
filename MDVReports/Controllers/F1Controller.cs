8c4d276993ba453bbccf756cd07b1efa5f714282
Sokolov <sokolov_ea@farmsib.ru>
2020-04-01 19:46:38 +0300

    q

--- MDVReports/Controllers/F1Controller.cs
+++ MDVReports/Controllers/F1Controller.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
 using System.Web.Mvc;
 
 namespace FNet.Settings.Controllers
@@ -7,8 +10,24 @@ namespace FNet.Settings.Controllers
     {
         public Object Index()
         {
-            Object v = "FNet.MDVReports.Controllers.F1Controller.Index()";
-            return v;
+            Object result = "FNet.MDVReports.Controllers.F1Controller.Index()";
+            String cnString = "Data Source=192.168.135.14;Initial Catalog=Grls;Integrated Security=True";
+            SqlConnection cn = new SqlConnection(cnString);
+            SqlCommand cmd = new SqlCommand()
+            {
+                Connection = cn,
+                CommandType = System.Data.CommandType.StoredProcedure,
+                CommandText = "[Ссылки на РУ сравнение дат]"
+            };
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            try
+            {
+                da.Fill(dt);
+                result = PartialView("~/Views/F1/Index.cshtml", dt);
+            }
+            catch (Exception e) { result += "<br>" + e.Message; }
+            return result;
         }
     }
 }
